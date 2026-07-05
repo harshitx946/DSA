79ce36a745e4a1122562ab864d95ae5367887636
harshitx946 <harshitx946@gmail.com>
2026-07-05 14:17:34 +0530

    Time: 0 ms (100.00%) | Memory: 21 MB (65.64%) - LeetSync

---
+++ 113-path-sum-ii/path-sum-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<int> path;
+
+    void dfs(TreeNode* root, int targetSum) {
+
+        if (root == NULL)
+            return;
+
+        path.push_back(root->val);
+
+        targetSum -= root->val;
+
+        if (root->left == NULL && root->right == NULL) {
+
+            if (targetSum == 0) {
+                ans.push_back(path);
+            }
+        }
+
+        dfs(root->left, targetSum);
+        dfs(root->right, targetSum);
+
+        path.pop_back();
+    }
+
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+
+        dfs(root, targetSum);
+
+        return ans;
+    }
+};
