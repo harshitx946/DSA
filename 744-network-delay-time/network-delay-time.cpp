72c8075c42a1760a5dfcc6896160c8765a9a2e72
harshitx946 <harshitx946@gmail.com>
2026-07-11 23:46:11 +0530

    Time: 75 ms (86.24%) | Memory: 44.5 MB (53.18%) - LeetSync

---
+++ 744-network-delay-time/network-delay-time.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+
+        vector<vector<pair<int, int>>> adj(n + 1);
+
+        // Build graph
+        for (auto &time : times) {
+            adj[time[0]].push_back({time[1], time[2]});
+        }
+
+        vector<int> dist(n + 1, INT_MAX);
+        dist[k] = 0;
+
+        priority_queue<pair<int, int>,
+                       vector<pair<int, int>>,
+                       greater<pair<int, int>>> pq;
+
+        pq.push({0, k});
+
+        while (!pq.empty()) {
+
+            auto [currDist, node] = pq.top();
+            pq.pop();
+
+            if (currDist > dist[node])
+                continue;
+
+            for (auto &neighbor : adj[node]) {
+
+                int nextNode = neighbor.first;
+                int weight = neighbor.second;
+
+                if (currDist + weight < dist[nextNode]) {
+
+                    dist[nextNode] = currDist + weight;
+
+                    pq.push({dist[nextNode], nextNode});
+                }
+            }
+        }
+
+        int ans = 0;
+
+        for (int i = 1; i <= n; i++) {
+
+            if (dist[i] == INT_MAX)
+                return -1;
+
+            ans = max(ans, dist[i]);
+        }
+
+        return ans;
+    }
+};
